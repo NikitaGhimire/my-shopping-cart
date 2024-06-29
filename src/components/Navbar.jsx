a8b5853cd421ff4202eccb8eeb21895ca3e373be
NikitaGhimire <nikitaghimire1@gmail.com>
2024-06-29 14:26:26 +1000

    Basic react project setup with components and pages mock

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navbar = ({cartCount}) => (
+    <nav>
+        <ul>
+            <li> <Link to="/">Home</Link></li>
+            <li> <Link to="/Shop">Shop</Link></li>
+            <li> Cart: {cartCount}</li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
