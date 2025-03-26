86f2963a6b19b0ef9b2defb817dcdd4f04289669
NikitaGhimire <nikitaghimire1@gmail.com>
2025-03-26 15:35:36 +1100

    Update core app configuration and entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Home from './pages/Home.jsx'
-import Shop from './pages/Shop.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-
-
-  children: [
-      { path: "/", element: <Home /> },
-      { path: "shop", element: <Shop /> },
-    ],
-  },
-]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
